00a861de2aab997fdf8c09377122421311a1ce45
PopekaDS <popekads@yandex.ru>
2024-09-29 22:24:54 +0400

    Обновлена форма, удалены поля для освещенности

--- Qt/mainwindow.cpp
+++ Qt/mainwindow.cpp
@@ -3,25 +3,13 @@
 
 #include <QMessageBox>
 #include <QtSerialPort/QSerialPort>
-#include <QTimer>
-  #include <QSerialPortInfo>
+#include <QSerialPortInfo>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-
-    // создали таймер, привязали его к форме
-    // если ругается на QTimer ставим на него курсор и нажимаем Alt+Enter
-    // чтоб на автомате добавить #include <QTimer>
-    QTimer *timer = new QTimer(this);
-
-    // подключили сигнал timeout, к слоту нажатия на кнопку
-    connect(timer, SIGNAL(timeout()), this, SLOT(on_pushButton_3_clicked()));
-
-    // запускаем со значением вызывать раз в 300мс
-    timer->start(300);
 
     foreach (const QSerialPortInfo &serialPortInfo, QSerialPortInfo::availablePorts())
     {
@@ -105,38 +93,6 @@ void MainWindow::on_pushButton_2_clicked()
     }
 
     ui->txtOutput->append(data);
-
-    serialPort.close();
-}
-
-
-void MainWindow::on_pushButton_3_clicked()
-{
-    QSerialPort serialPort;
-
-    serialPort.setPortName(this->ui->cmbPort->currentText());
-    serialPort.setBaudRate(QSerialPort::Baud9600);
-
-//    if (!serialPort.open(QIODevice::ReadWrite)) {
-//        QMessageBox::warning(this, "Ошибка", "Не удалось подключится к порту");
-//        return;
-//    }
-    if (!serialPort.open(QIODevice::ReadWrite)) {
-        // закомментим вызовы QMessageBox, так как эта функция будет вызываться таймером
-        // и информационное сообщение будет блокировать работу программы
-        //QMessageBox::warning(this, "Ошибка", "Не удалось подключится к порту");
-        return; // просто оставляем ретёрн, типа не подключились ну и бог с ним
-    }
-
-    serialPort.write("p"); // меняем тут на p
-    serialPort.waitForBytesWritten();
-
-    QByteArray data;
-    while (serialPort.waitForReadyRead(10)) {
-        data.append(serialPort.readAll());
-    }
-
-    ui->txtLight->setText(data); // тут вставляем значение в txtLight
 
     serialPort.close();
 }
