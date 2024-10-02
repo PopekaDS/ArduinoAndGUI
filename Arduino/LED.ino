df0e7da31c551b3a171c1ff9a9a1c4b1c05aeabd
PopekaDS <popekads@yandex.ru>
2024-10-02 21:15:12 +0400

    Исправлена ошибка с отображением на дисплее 16 и менее символов

--- Arduino/LED.ino
+++ Arduino/LED.ino
@@ -1,14 +1,4 @@
 /*
-  LiquidCrystal Library - Blink
-
- Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
- library works with all LCD displays that are compatible with the
- Hitachi HD44780 driver. There are many of them out there, and you
- can usually tell them by the 16-pin interface.
-
- This sketch prints "Hello World!" to the LCD and makes the
- cursor block blink.
-
  The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
@@ -21,54 +11,41 @@
    * ends to +5V and ground
    * wiper to LCD VO pin (pin 3)
 
- Library originally added 18 Apr 2008
- by David A. Mellis
- library modified 5 Jul 2009
- by Limor Fried (http://www.ladyada.net)
- example added 9 Jul 2009
- by Tom Igoe
- modified 22 Nov 2010
- by Tom Igoe
- modified 7 Nov 2016
- by Arturo Guadalupi
-
  This example code is in the public domain.
 
  http://www.arduino.cc/en/Tutorial/LiquidCrystalBlink
-
 */
 
-// include the library code:
 #include <LiquidCrystal.h>
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
+char checkMove = '0';
+char string[50];
+
 void setup() {
-  // set up the LCD's number of columns and rows:
   Serial.begin(9600);
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.clear();
   lcd.print("wait ...");
 }
 
 void loop() {
     if (Serial.available() > 0) {
       lcd.clear();
-      char check = '1';
-      check = Serial.read();
+      
+      // Проверяем первый байт, если
+      // 0 - строка на дисплее стоит на месте
+      // 1 - строка на дисплее будет двигаться влево
+      checkMove = Serial.read();
 
-      char string[50];
-      if (check == '0') {
+      if (checkMove == '0') {
         for (int i = 0; i < 16; i++)
         {
           delay(2);
           string[i] = Serial.read();
           if(int(string[i]) == -1) {
-            string[i] = ' ';
+            string[i] = 0;
           }
 
           Serial.print(char(string[i]));
@@ -76,7 +53,6 @@ void loop() {
         string[16] = 0;
         lcd.setCursor(0, 0);
         lcd.print(string);
-
       } else {
         delay(2);
         char f = Serial.read();
@@ -99,7 +75,7 @@ void loop() {
         string[size] = 0;
         lcd.setCursor(0, 0);
         lcd.print(string);
-
+/*
         // scroll 13 positions (string length) to the left
         // to move it offscreen left:
         for (int positionCounter = 0; positionCounter < size; positionCounter++) {
@@ -107,12 +83,19 @@ void loop() {
           lcd.scrollDisplayLeft();
           // wait a bit:
           delay(500);
-        }
+        }*/
 
       // delay at the end of the full loop:
       delay(1000);
+      lcd.setCursor(0, 1);
+        lcd.print(checkMove);
       }
+      
   }
+
+  /*
+если чек = 1 то сделать сдвиг
+  */
 }
 
 
