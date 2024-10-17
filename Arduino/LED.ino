a6d4b88bcf47badc4da6498c2fdfc93c120a75c4
PopekaDS <popekads@yandex.ru>
2024-10-17 23:06:26 +0400

    Убраны лишние участки кода, которые не вляли на работу программы

--- Arduino/LED.ino
+++ Arduino/LED.ino
@@ -1,19 +1,15 @@
 /*
- The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * 10K resistor:
-   * ends to +5V and ground
-   * wiper to LCD VO pin (pin 3)
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/LiquidCrystalBlink
+* The circuit:
+* LCD RS pin to digital pin 12
+* LCD Enable pin to digital pin 11
+* LCD D4 pin to digital pin 5
+* LCD D5 pin to digital pin 4
+* LCD D6 pin to digital pin 3
+* LCD D7 pin to digital pin 2
+* LCD R/W pin to ground
+* 10K resistor:
+* ends to +5V and ground
+* wiper to LCD VO pin (pin 3)
 */
 
 #include <LiquidCrystal.h>
@@ -21,12 +17,15 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-bool checkMove2 = false;
+// Если checkScroll равен true, то LCD будет делать
+// бегущую строку, это требуется для длинных сообщений
+// Если checkScroll равен false, то LCD просто строку без сдвигов
+bool checkScroll = false;
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
-  lcd.print("wait ...");
+  lcd.print("hello, world!");
 }
 
 void loop() {
@@ -36,13 +35,11 @@ void loop() {
       // Проверяем первый байт, если
       // 0 - строка на дисплее стоит на месте
       // 1 - строка на дисплее будет двигаться влево
-      char checkMove = Serial.read();
-      Serial.print(checkMove);
-      Serial.print(' ');
+      char firstByte = Serial.read();
       delay(2);
 
-      if (checkMove == '0') {
-        checkMove2 = false;
+      if (firstByte == '0') {
+        checkScroll = false;
         char string[16];
 
         for (int i = 0; i < 16; i++)
@@ -59,19 +56,9 @@ void loop() {
         lcd.setCursor(0, 0);
         lcd.print(string);
       } else {
-        checkMove2 = true;
+        checkScroll = true;
         char string[30];
-        
-        // delay(2);
-        // char f = Serial.read();
-        // Serial.print(f);
-        // delay(2);
-        // char s = Serial.read();
-        // Serial.print(s);
-        // Serial.print(' ');
-        // delay(2);
 
-        // int size = (int(f - '0')) * 10 + int(s - '0');
         for (int i = 0; i < 30; i++)
         {
           delay(2);
@@ -86,29 +73,10 @@ void loop() {
         string[30] = 0;
         lcd.setCursor(0, 0);
         lcd.print(string);
-/*
-        // scroll 13 positions (string length) to the left
-        // to move it offscreen left:
-        for (int positionCounter = 0; positionCounter < size; positionCounter++) {
-          // scroll one position left:
-          lcd.scrollDisplayLeft();
-          // wait a bit:
-          delay(500);
-        }*/
-
-      // delay at the end of the full loop:
       }
       delay(1000);  
-  } else if(checkMove2) {
-  lcd.scrollDisplayLeft();
-  delay(500);
+  } else if(checkScroll) {
+    lcd.scrollDisplayLeft();
+    delay(500);
+  }
 }
-
-  /*
-если чек = 1 то сделать сдвиг
-  */
-
-
-}
-
-
